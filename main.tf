2d13b47ca9148063a9f500127ae91cf6f9b46a54
charles-d-burton <charles.burton@mindflash.com>
2017-11-02 11:12:12 -0600

    removed ingress/egress rules in favor of security_group_rule resources

--- main.tf
+++ main.tf
@@ -4,20 +4,6 @@ resource "aws_security_group" "internal" {
   count  = "${var.internal_lb}"
   name   = "tf-alb-sg-${var.name}-internal"
   vpc_id = "${var.vpc_id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["${data.aws_vpc.selected.cidr_block}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["${data.aws_vpc.selected.cidr_block}"]
-  }
 }
 
 resource "aws_lb" "internal_alb" {
@@ -28,24 +14,30 @@ resource "aws_lb" "internal_alb" {
   subnets         = ["${var.private_subnets}"]
 }
 
+resource "aws_security_group_rule" "internal_egress" {
+  count             = "${var.internal_lb}"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.internal.id}"
+}
+
 resource "aws_security_group" "external" {
   count  = "${var.external_lb}"
   name   = "tf-alb-sg-${var.name}-external"
   vpc_id = "${var.vpc_id}"
+}
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "external_egress" {
+  count             = "${var.external_lb}"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.external.id}"
 }
 
 resource "aws_lb" "external_alb" {
