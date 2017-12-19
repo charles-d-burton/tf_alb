bad9e9419551bbb51b6a7730fc3814def6f32438
charles-d-burton <charles.burton@mindflash.com>
2017-12-19 07:43:57 -0700

    updated egress rule

--- main.tf
+++ main.tf
@@ -17,9 +17,9 @@ resource "aws_lb" "internal_alb" {
 resource "aws_security_group_rule" "internal_egress" {
   count             = "${var.internal_lb}"
   type              = "egress"
-  from_port         = 1
-  to_port           = 65535
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.internal.id}"
 }
