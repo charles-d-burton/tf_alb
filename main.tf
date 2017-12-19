9b3293171fcb6f4f2862dbd4d26d5e76ea08bcc4
charles-d-burton <charles.burton@mindflash.com>
2017-12-19 07:40:57 -0700

    updated egress rule

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group_rule" "external_egress" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
-  protocol          = "tcp"
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = "${aws_security_group.external.id}"
 }
