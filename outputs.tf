bf20fdb1413d653260c869600a2a75a7febbfb83
charles-d-burton <charles.burton@mindflash.com>
2018-03-07 14:35:11 -0700

    fix: output syntax per TF 11.3

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,47 @@
 output "internal_alb_arn" {
-  value = "${aws_lb.internal_alb.arn}"
+  value = "${aws_lb.internal_alb.*.arn}"
 }
 
 output "internal_alb_arn_suffix" {
-  value = "${aws_lb.internal_alb.arn_suffix}"
+  value = "${aws_lb.internal_alb.*.arn_suffix}"
 }
 
 output "internal_alb_dns_name" {
-  value = "${aws_lb.internal_alb.dns_name}"
+  value = "${aws_lb.internal_alb.*.dns_name}"
 }
 
 output "internal_canonical_hosted_zone_id" {
-  value = "${aws_lb.internal_alb.canonical_hosted_zone_id}"
+  value = "${aws_lb.internal_alb.*.canonical_hosted_zone_id}"
 }
 
 output "internal_zone_id" {
-  value = "${aws_lb.internal_alb.zone_id}"
+  value = "${aws_lb.internal_alb.*.zone_id}"
 }
 
 output "external_alb_arn" {
-  value = "${aws_lb.external_alb.arn}"
+  value = "${aws_lb.external_alb.*.arn}"
 }
 
 output "external_alb_arn_suffix" {
-  value = "${aws_lb.external_alb.arn_suffix}"
+  value = "${aws_lb.external_alb.*.arn_suffix}"
 }
 
 output "external_alb_dns_name" {
-  value = "${aws_lb.external_alb.dns_name}"
+  value = "${aws_lb.external_alb.*.dns_name}"
 }
 
 output "external_canonical_hosted_zone_id" {
-  value = "${aws_lb.external_alb.canonical_hosted_zone_id}"
+  value = "${aws_lb.external_alb.*.canonical_hosted_zone_id}"
 }
 
 output "external_zone_id" {
-  value = "${aws_lb.external_alb.zone_id}"
+  value = "${aws_lb.external_alb.*.zone_id}"
 }
 
 output "internal_security_group_id" {
-  value = "${aws_security_group.internal.id}"
+  value = "${aws_security_group.*.internal.id}"
 }
 
 output "external_security_group_id" {
-  value = "${aws_security_group.external.id}"
+  value = "${aws_security_group.*.external.id}"
 }
